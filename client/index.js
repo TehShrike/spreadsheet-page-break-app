acfc924dd94bd28995c2fd8c5eb91ee75e729e23
Josh Duff <me@JoshDuff.com>
2017-02-08 20:59:32 -0600

    A dumb start

---
+++ client/index.js
@@ -0,0 +1,65 @@
+require('babel-polyfill')
+
+const StateRouter = require('abstract-state-router')
+const makeSvelteStateRenderer = require('svelte-state-renderer')
+const mannish = require('mannish')
+
+const mediator = mannish()
+
+const renderer = makeSvelteStateRenderer({
+	data: {
+		call: mediator.call
+	}
+})
+
+const context = {
+	mediator,
+	config: {
+		googleApiClientId: '701826345256-37j3kdn4dmn40moje64l7poabn5cg0a2.apps.googleusercontent.com'
+	}
+}
+
+const stateRouter = StateRouter(renderer, document.getElementById('container'))
+
+mediator.provide('stateGo', stateRouter.go)
+
+stateRouter.addState({
+	name: 'sheetsSignIn',
+	route: 'sheets-sign-in',
+	template: require('./component/sheetsSignIn.html')
+})
+
+stateRouter.addState({
+	name: 'app',
+	route: 'app',
+	template: require('./component/app.html'),
+	resolve: async (data, parameters) => {
+		const signedIn = await mediator.call('gapi isSignedIn')
+
+		if (signedIn) {
+			return Promise.resolve({})
+		} else {
+			return Promise.reject({
+				redirectTo: {
+					name: 'sheetsSignIn'
+				}
+			})
+		}
+	}
+})
+
+stateRouter.addState({
+	name: 'app.selectSpreadsheet',
+	route: 'select-spreadsheet',
+	template: require('./component/selectSpreadsheet.html')
+})
+
+const statefulModules = [
+	require('./service/googleApi')
+]
+
+const launchingAllModules = Promise.all(statefulModules.map(module => module(context)))
+
+launchingAllModules.then(() => {
+	stateRouter.evaluateCurrentRoute('app.selectSpreadsheet')
+})
---
+++ client/service/googleApi.js
@@ -0,0 +1,86 @@
+const EventEmitter = require('eventemitter')
+const waterfall = require('p-waterfall')
+
+const DISCOVERY_DOCS = [ 'https://sheets.googleapis.com/$discovery/rest?version=v4' ]
+const SCOPES = 'https://www.googleapis.com/auth/spreadsheets.readonly'
+
+module.exports = async function googleApiWrapper({ config, mediator }) {
+	const googleApi = gapi // eslint-disable-line no-undef
+
+	const internalApi = await createGoogleApiWrapper(googleApi, config.googleApiClientId)
+
+	Object.keys(internalApi).forEach(functionName => {
+		const fn = internalApi[functionName]
+
+		mediator.provide(`gapi ${functionName}`, fn)
+	})
+}
+
+async function createGoogleApiWrapper(googleApi, clientId) {
+	const initializationPromise = await initializeGoogleApi(googleApi, clientId)
+
+	const initializeFn = () => initializationPromise
+
+	const signedInWatcher = createSignedInWatcher(googleApi)
+
+	return {
+		signIn: () => waterfall([
+			initializeFn,
+			async () => {
+				googleApi.auth2.getAuthInstance().signIn()
+
+				await signedInWatcher.awaitSignIn()
+			}
+		]),
+		isSignedIn: () => waterfall([ initializeFn, signedInWatcher.isSignedIn ])
+	}
+}
+
+async function initializeGoogleApi(googleApi, clientId) {
+	await gapiAwaitLoadClient(googleApi)
+
+	await googleApi.client.init({
+		discoveryDocs: DISCOVERY_DOCS,
+		clientId: clientId,
+		scope: SCOPES
+	})
+}
+
+function createSignedInWatcher(googleApi) {
+	const emitter = new EventEmitter()
+	let signedIn = gapiIsSignedIn(googleApi)
+	console.log('signed in state initialized to', signedIn)
+
+	googleApi.auth2.getAuthInstance().isSignedIn.listen(newSignedInState => {
+		signedIn = newSignedInState
+
+		console.log('signed in state is now', newSignedInState)
+
+		if (newSignedInState) {
+			emitter.emit('signed in')
+		} else {
+			emitter.emit('signed out')
+		}
+	})
+
+	return {
+		awaitSignIn() {
+			if (signedIn) {
+				return Promise.resolve()
+			} else {
+				return new Promise(resolve => emitter.once('signed in', resolve))
+			}
+		},
+		isSignedIn() {
+			return signedIn
+		}
+	}
+}
+
+function gapiAwaitLoadClient(googleApi) {
+	return new Promise(resolve => googleApi.load('client:auth2', resolve))
+}
+
+function gapiIsSignedIn(googleApi) {
+	return googleApi.auth2.getAuthInstance().isSignedIn.get()
+}
